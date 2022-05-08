a5a53305689e4d2c4aa0dd63962f6c5a46b95885
ihpolyphe <hayato.8124950@mineo.jp>
2022-05-08 23:12:22 +0900

    add ESLint

--- APImysql/index.js
+++ APImysql/index.js
@@ -7,11 +7,11 @@ app.use(express.static('public'));
 
 //Mysql設定
 const connection = mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'Route246',
-    database:'ikku',
-    port:3306
+  host: 'localhost',
+  user: 'root',
+  password: 'Route246',
+  database: 'ikku',
+  port: 3306,
 });
 
 // FetchAPI設定
@@ -19,65 +19,63 @@ const jsonParser = bodyParser.json();
 
 // create
 app.post('/fetch', jsonParser, (req, res) => {
-    const body = req.body;
-    const ikku = req.body.ikku;
+  const body = req.body;
+  const ikku = req.body.ikku;
 
-    const insertSql = 'INSERT INTO ikkulist SET ?';
-    const selectSql = 'SELECT * FROM ikkulist WHERE ikku = ?';
-   
-    connection.query( insertSql, body, (err, result) => {
-        if(err) throw err;
-        connection.query(selectSql, ikku, (err, result) => {
-            if(err) throw err;
-            console.log(result[0]);
-            res.send(result[0]);
-       });
-   });
+  const insertSql = 'INSERT INTO ikkulist SET ?';
+  const selectSql = 'SELECT * FROM ikkulist WHERE ikku = ?';
+
+  connection.query(insertSql, body, (err, result) => {
+    if (err) throw err;
+    connection.query(selectSql, ikku, (err, result) => {
+      if (err) throw err;
+      console.log(result[0]);
+      res.send(result[0]);
+    });
+  });
 });
-
-// read
+// *!read
 app.get('/fetch', jsonParser, (req, res) => {
-    const selectSql = 'SELECT * FROM ikkulist';
-        connection.query(selectSql, (err, result) => {
-    if(err) throw err;
+  const selectSql = 'SELECT * FROM ikkulist';
+  connection.query(selectSql, (err, result) => {
+    if (err) throw err;
     console.log(result);
-        res.send(result);
-        });
+    res.send(result);
+  });
 });
 
 // update
-app.put('/fetch', jsonParser, (req,res) => {
-    const id = req.body.id;
-    const ikku = req.body.ikku;
+app.put('/fetch', jsonParser, (req, res) => {
+  const id = req.body.id;
+  const ikku = req.body.ikku;
 
-    const updateSql = "UPDATE ikkulist SET ikku = ? WHERE id = ?";
-    const selectSql = "SELECT * FROM ikkulist WHERE id = ?";
+  const updateSql = 'UPDATE ikkulist SET ikku = ? WHERE id = ?';
+  const selectSql = 'SELECT * FROM ikkulist WHERE id = ?';
 
-    connection.query(updateSql, [ikku, id], (err, result) => {
-        if(err) throw err;
-        connection.query(selectSql, id, (err, result) => {
-            if(err) throw err;
-            console.log(result[0]);
-            res.send(result[0]);
-        });
+  connection.query(updateSql, [ikku, id], (err, result) => {
+    if (err) throw err;
+    connection.query(selectSql, id, (err, result) => {
+      if (err) throw err;
+      console.log(result[0]);
+      res.send(result[0]);
     });
+  });
 });
 
 //delete
-app.delete('/fetch', jsonParser, (req,res) => {
-    const id = req.body.id;
-    const deleteSql = 'DELETE FROM ikkulist WHERE id = ?';
+app.delete('/fetch', jsonParser, (req, res) => {
+  const id = req.body.id;
+  const deleteSql = 'DELETE FROM ikkulist WHERE id = ?';
 
-    connection.query(deleteSql, id , (err, result) => {
-        if(err) throw err;
-        res.json({
-            'id':Number(id),
-            'ikku':'deleted'
-        });
+  connection.query(deleteSql, id, (err, result) => {
+    if (err) throw err;
+    res.json({
+      id: Number(id),
+      ikku: 'deleted',
     });
+  });
 });
 
-
 app.listen(3000, () => {
-    console.log('Start server port:3000');
+  console.log('Start server port:3000');
 });
--- README.md
+++ README.md
@@ -1,30 +1,34 @@
 # NodeJs turtorial
 
-Node.jsを勉強するための練習
+Node.js を勉強するための練習
 作成したアプリ
+
 - simpleChat:非同期な簡単なチャット機能を作成
-- mysql:Mysqlの練習ディレクトリ。データベースを作成してアクセスできるところまでできている。
+- mysql:Mysql の練習ディレクトリ。データベースを作成してアクセスできるところまでできている。
 
 # Todo
+
 - [x] フロントエンドの改良
-→ cssの設定と読み込めるようにするよう改良(今後さらにUI改良する)
+      → css の設定と読み込めるようにするよう改良(今後さらに UI 改良する)
 
-[【Express】静的HTMLファイルの表示（res.sendFile()、express.static()）](https://www.i-ryo.com/entry/2020/04/16/215205)
+[【Express】静的 HTML ファイルの表示（res.sendFile()、express.static()）](https://www.i-ryo.com/entry/2020/04/16/215205)
 
-- [x] [Node.js / Expressを習得するためにやったことまとめ（随時更新）](https://qiita.com/i-ryo/items/d7416e0c4a6035c30288)を読む
-→Expressを使用したCRUD処理まで実装できたのでいったん区切り。
+- [x] [Node.js / Express を習得するためにやったことまとめ（随時更新）](https://qiita.com/i-ryo/items/d7416e0c4a6035c30288)を読む
+      →Express を使用した CRUD 処理まで実装できたのでいったん区切り。
 
-- [ ] REST APIの勉強
+- [ ] REST API の勉強
 
-- [ ] glitchでwebサーバーに公開
-- [x] コマンドプロンプトでの開発が煩わしいのでWSLでNode.js開発できるようにセットアップする。
-- [x] README.md修正（ここはNode.jsの話、simpleChatにREADME.md追加）
+- [ ] glitch で web サーバーに公開
+- [x] コマンドプロンプトでの開発が煩わしいので WSL で Node.js 開発できるようにセットアップする。
+- [x] README.md 修正（ここは Node.js の話、simpleChat に README.md 追加）
 
-## Node.js環境構築(winodows)
-- [Node.jsの開発環境を用意しよう！（Windows）](https://prog-8.com/docs/nodejs-env-win)
+## Node.js 環境構築(winodows)
+
+- [Node.js の開発環境を用意しよう！（Windows）](https://prog-8.com/docs/nodejs-env-win)
 - [Yarn を導入しよう（Windows）](https://qiita.com/kurararara/items/21c70c4adfd3bb323412)
 
 ## クローンして動かす方法
+
 ```
 ## リモートリポジトリからクローン
 $ git clone https://github.com/ihpolyphe/simpleChat
@@ -41,5 +45,9 @@ $ node index.js
 
 起動に成功すると、ブラウザで`http://localhost:3000/`でチャットボットが起動できる。
 
-## Javascript拡張機能
+## Javascript 拡張機能
 
+- ESlint:静的チェックツール
+- Prettier:自動成型ツール
+
+参照:[【初心者向け】Visual Studio Code へ ESLint と Prettier を導入する](https://qiita.com/genbu-jp/items/a48bcb9df209b71c2de3)
